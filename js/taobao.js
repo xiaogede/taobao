a3e4a3236947843f5f66813a47f101bba71d4135
xiaogede <2469136989@qq.com>
2017-12-26 20:19:13 +0800

    上传

---
+++ js/taobao.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lenovo on 17/12/21 021.
+ */
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    autoplay:true,
+    pagination: {
+        el: '.swiper-pagination'
+    }
+
+}) ;
